6be14d974809a5ff2c99627f59485fce3d50a1d6
Artem <updatethatpl0x@gmail.com>
2023-01-20 20:54:54 -0500

    added chosen colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        plasma: {
+          100: "#D1FDFF",
+          500: "#01ECF7",
+          700: "#081D1E",
+        },
+        star: {
+          100: "#FEFEEF",
+          500: "#F9FF57",
+          700: "#30310D",
+        },
+        nebula: {
+          100: "#F5E4FE",
+          500: "#A104F8",
+          700: "#190A21",
+        },
+      },
+    },
   },
   plugins: [],
 };
