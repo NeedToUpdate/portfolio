4756d01bbe2f8427ca521289530f7c90d1b883b7
Artem <updatethatpl0x@gmail.com>
2023-01-23 11:42:09 -0500

    added tech icons for projects

---
+++ components/techIcon.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Image from "next/image";
+
+export type TechType = "react" | "tailwind" | "numpy" | "postgresql" | "django" | "sass" | "nextjs" | "vuejs" | "html" | "mongodb" | "nodejs" | "python" | "typescript";
+
+interface props {
+  tech: TechType;
+  size?: number;
+}
+
+export default function TechIcon(props: props) {
+  const { size, tech } = props;
+  return (
+    <div className="object-fit flex justify-center items-center h-fit p-[2px] rounded-sm">
+      <Image title={`Made with ${tech[0].toLocaleUpperCase() + tech.slice(1)}.`} alt={tech} width={size || 12} height={size || 12} src={"/icons/" + tech + "-icon.png"}></Image>
+    </div>
+  );
+}
