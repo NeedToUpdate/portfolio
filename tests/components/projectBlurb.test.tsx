6278709884a32a4b6da0657fc2d75516b6769797
Artem <updatethatpl0x@gmail.com>
2023-01-30 13:48:41 -0500

    attribute name change: image -> thumbnail

--- tests/components/projectBlurb.test.tsx
+++ tests/components/projectBlurb.test.tsx
@@ -3,7 +3,7 @@ import { render, screen, waitFor } from "@testing-library/react";
 import ProjectBlurb from "../../components/projectBlurb";
 describe("<ProjectBlurb/>", () => {
   it("displays the title and description", async () => {
-    render(<ProjectBlurb title={"Test Title"} description={"Test Description"} techs={["angular"]} image={"/icons/angular-icon.png"} />);
+    render(<ProjectBlurb url={""} title={"Test Title"} description={"Test Description"} techs={["angular"]} thumbnail={"/icons/angular-icon.png"} />);
     expect(screen.getByRole("heading", { name: /Test Title/i })).toBeVisible();
     expect(screen.getByRole("note")).toBeVisible();
   });
