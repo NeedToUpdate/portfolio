738021673600712d75e91184aa01369f96ace57e
Artem <updatethatpl0x@gmail.com>
2025-02-01 19:39:21 -0500

    moved skills to its own file

---
+++ components/utils/skills.ts
@@ -0,0 +1,64 @@
+import { ISkill } from "./types";
+
+export const skills:ISkill[] = [
+    {
+      tech: "react",
+      prettyName: "ReactJS",
+      years: 4,
+    },
+    {
+      tech: "typescript",
+      prettyName: "TypeScript",
+      years: 6,
+    },
+    {
+      tech: "python",
+      prettyName: "Python",
+      years: 6,
+    },
+    {
+      tech: "html",
+      prettyName: "HTML5",
+      years: 6,
+    },
+    {
+      tech: "sass",
+      prettyName: "CSS/SCSS",
+      years: 6,
+    },
+    {
+      tech: "django",
+      prettyName: "Django",
+      years: 3,
+    },
+    {
+      tech: "nodejs",
+      prettyName: "Node.js",
+      years: 4,
+    },
+    {
+      tech: "vuejs",
+      prettyName: "VueJS",
+      years: 3,
+    },
+    {
+      tech: "postgresql",
+      prettyName: "PostgreSQL",
+      years: 3,
+    },
+    {
+      tech: "mongodb",
+      prettyName: "MongoDB",
+      years: 3,
+    },
+    {
+      tech: "nextjs",
+      prettyName: "NextJS",
+      years: 2,
+    },
+    {
+      tech: "aws",
+      prettyName: "AWS",
+      years: 2,
+    },
+  ];
